398735b8a0350b931caf4cd503077f7d2d92c86f
Naif <naif--556677@outlook.sa>
2024-02-12 10:00:26 +0300

    initial

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template,url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("dashboard.html") #go to dashboard by default
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/registerdUsers")
+def registerdUsers():
+    return render_template("registerdUsers.html")
+
+@app.route("/history")
+def history():
+    return render_template("history.html")
+
+@app.route("/newUser")
+def newUser():
+    return render_template("newUser.html")
+
+@app.route("/logout")
+def logout():
+    return render_template("logout.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
