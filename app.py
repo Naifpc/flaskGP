3eda4a6e006121403fd18f4c243f4e8f3fb09b94
Naif <naif--556677@outlook.sa>
2024-02-21 13:08:57 +0300

    modified:   app.py modified:   static/css/main.css modified:   templates/index.html modified:   templates/login.html

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, url_for, request, redirect,session
+#import sqlalchemy
+from datetime import timedelta
 
 
 
 app = Flask(__name__)
+app.secret_key = "graduate"
+app.permanent_session_lifetime = timedelta(days=1) #keep session for one day
 
 @app.route("/")
 def index():
@@ -13,13 +17,21 @@ def login():
     if request.method == "POST":
         user = request.form["username"]
         password = request.form["password"]
-        return redirect(url_for("dashboard", usr=user, pw=password))
+        session["user"]=user
+        session["password"]=password # get username and password and save in session then go to dashboard
+        return redirect(url_for("dashboard"))
     else:
+        if "user" in session:
+           return redirect(url_for("dashboard")) #if user already in Session go to Dashboared
         return render_template("login.html") 
 
-@app.route("/dashboard/<usr>/<pw>")
-def dashboard(usr, pw):
-    return render_template("dashboard.html", username=usr, password=pw)
+@app.route("/dashboard")
+def dashboard():
+    if "user" in session: 
+        user = session["user"]
+        return render_template("dashboard.html",username = user)
+    else:
+        return redirect(url_for("login"))
 
 @app.route("/registerdUsers")
 def registerdUsers():
@@ -32,6 +44,12 @@ def history():
 @app.route("/newUser")
 def newUser():
     return render_template("newUser.html")
+
+@app.route("/logout") #Delete username and password from Session then go to login
+def logout():
+    session.pop("user",None)
+    session.pop("password",None)
+    return redirect(url_for("login"))
 
 
 if __name__ == "__main__":
